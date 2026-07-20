5554120a9fa0f3966e1cf30b09b1ae9f0b917210
Molly Venegas <mollyvenegasvasquez@gmail.com>
2026-07-21 11:45:42 +1200

    Primer proyecto React del portafolio

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hola, soy Molly 👋</h1>
+
+      <h2>Desarrolladora Web Frontend</h2>
+
+      <p>
+        Estoy aprendiendo React para crear páginas web modernas.
+      </p>
+
+      <p>Mi objetivo es trabajar creando páginas web desde casa.
+      </p>
+
+      <button>Ver proyectos</button>
+    </div>
+  );
+}
+
+export default App;
